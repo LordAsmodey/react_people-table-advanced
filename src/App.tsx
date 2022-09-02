038997cc9f1ac552ca3f656be943c0f6fe1e3eb3
Oleg Vetrov <oleg94038@gmail.com>
2022-09-02 14:10:53 +0300

    Added people table implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage';
 import { Navbar } from './components/Navbar';
+import { PageNotFound } from './components/PageNotFound';
+import { HomePage } from './components/HomePage';
 
 export const App = () => {
   return (
@@ -8,9 +11,15 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,4 +1,32 @@
-export const PeopleTable = () => {
+import React, { useState } from 'react';
+import cn from 'classnames';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = (props) => {
+  const { people } = props;
+  const [selectedPersonSlug, setSelectedPersonSlug] = useState('');
+
+  const getParent = (parentName: string | null) => {
+    if (parentName) {
+      return people.find(person => person.name === parentName);
+    }
+
+    return undefined;
+  };
+
+  const preparedPersonList = people.map(person => (
+    {
+      ...person,
+      mother: getParent(person.motherName),
+      father: getParent(person.fatherName),
+    }
+  ));
+
   return (
     <table
       data-cy="peopleTable"
@@ -56,20 +84,60 @@ export const PeopleTable = () => {
       </thead>
 
       <tbody>
-        <tr data-cy="person">
-          <td>
-            <a href="#/people/pieter-haverbeke-1602">Pieter Haverbeke</a>
-          </td>
-          <td>m</td>
-          <td>1602</td>
-          <td>1642</td>
-          <td>-</td>
-          <td>
-            <a href="#/people/lieven-van-haverbeke-1570">
-              Lieven van Haverbeke
-            </a>
-          </td>
-        </tr>
+        {preparedPersonList.map(person => (
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={cn({
+              'has-background-warning': person.slug === selectedPersonSlug,
+            })}
+          >
+            <td>
+              <PersonLink
+                person={person}
+                selectPerson={setSelectedPersonSlug}
+              />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            {person.mother ? (
+              <td>
+                <PersonLink
+                  person={person.mother}
+                  selectPerson={setSelectedPersonSlug}
+                />
+              </td>
+            ) : (
+              <td>
+                {person.motherName ? (
+                  person.motherName
+                ) : (
+                  '-'
+                )}
+              </td>
+            )}
+
+            {person.father ? (
+              <td>
+                <PersonLink
+                  person={person.father}
+                  selectPerson={setSelectedPersonSlug}
+                />
+              </td>
+            ) : (
+              <td>
+                {person.fatherName ? (
+                  person.fatherName
+                ) : (
+                  '-'
+                )}
+              </td>
+            )}
+
+          </tr>
+        ))}
 
         <tr data-cy="person">
           <td>
